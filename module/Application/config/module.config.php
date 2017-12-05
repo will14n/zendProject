32c430105fa0613984a8ed97c055794b2bd54a77
willianf4m <willianf4m@gmail.com>
2017-12-05 20:41:57 -0200

    setup de models e doctrine

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -57,4 +57,18 @@ return [
             __DIR__ . '/../view',
         ],
     ],
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ . '_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../src/Entity']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ]
+            ]
+        ]
+    ]  
 ];
