fae77297e24fac7cb23a5b4af79214800245c6a7
willianf4m <willianf4m@gmail.com>
2017-12-05 23:43:06 -0200

    arquivo configuração de view helpers

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -14,70 +14,69 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 use Application\Route\StaticRoute;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 
-
 return [
-	'router' => [
-		'routes' => [
-			'home' => [
-				'type' => Literal::class,
-				'options' => [
-					'route'    => '/',
-					'defaults' => [
-						'controller' => Controller\IndexController::class,
-						'action'     => 'index',
-					],
-				],
-			],
-			'application' => [
-				'type'    => Segment::class,
-				'options' => [
-					'route'    => '/application[/:action]',
-					'defaults' => [
-						'controller' => Controller\IndexController::class,
-						'action'     => 'index',
-					],
-				],
-			],
-			'posts' => [
-				'type'    => Segment::class,
-				'options' => [
-					'route'    => '/posts[/:action[/:id]]',
-					'constraints' => [
-						'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-						'id' => '[0-9]*'
-					],
-					'defaults' => [
-						'controller'    => Controller\PostController::class,
-						'action'        => 'index',
-					],
-				],
-			],
-			'contactus' => [
-				'type' => Literal::class,
-				'options' => [
-					'route'    => '/contactus',
-					'defaults' => [
-						'controller' => Controller\IndexController::class,
-						'action'     => 'contactUs',
-					],
-				],
-			],		 
-		],
-	],
-	'service_manager' => [
-		//...
-		'factories' => [
-			Service\PostManager::class => Service\Factory\PostManagerFactory::class,
-		],
-	],
-	'controllers' => [
-		//...
-		'factories' => [
-			Controller\IndexController::class => Controller\Factory\IndexControllerFactory::class,
-			 Controller\PostController::class => Controller\Factory\PostControllerFactory::class,
-		],
-	],
-	'view_helpers' => [
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'application' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/application[/:action]',
+                    'defaults' => [
+                        'controller'    => Controller\IndexController::class,
+                        'action'        => 'index',
+                    ],
+                ],
+            ],
+            'posts' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/posts[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]*'
+                    ],
+                    'defaults' => [
+                        'controller'    => Controller\PostController::class,
+                        'action'        => 'index',
+                    ],
+                ],
+            ],
+            'about' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/about',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'about',
+                    ],
+                ],
+            ],                                    
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => Controller\Factory\IndexControllerFactory::class,
+            Controller\PostController::class => Controller\Factory\PostControllerFactory::class,
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+            Service\PostManager::class => Service\Factory\PostManagerFactory::class,
+        ],
+    ],
+    // The following registers our custom view 
+    // helper classes in view plugin manager.
+    'view_helpers' => [
         'factories' => [
             View\Helper\Menu::class => InvokableFactory::class,
             View\Helper\Breadcrumbs::class => InvokableFactory::class,
@@ -87,34 +86,34 @@ return [
             'pageBreadcrumbs' => View\Helper\Breadcrumbs::class,
         ],
     ],
-	'view_manager' => [
-		'display_not_found_reason' => true,
-		'display_exceptions'       => true,
-		'doctype'                  => 'HTML5',
-		'not_found_template'       => 'error/404',
-		'exception_template'       => 'error/index',
-		'template_map' => [
-			'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-			'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-			'error/404'               => __DIR__ . '/../view/error/404.phtml',
-			'error/index'             => __DIR__ . '/../view/error/index.phtml',
-		],
-		'template_path_stack' => [
-			__DIR__ . '/../view',
-		],
-	],
-	'doctrine' => [
-		'driver' => [
-			__NAMESPACE__ . '_driver' => [
-				'class' => AnnotationDriver::class,
-				'cache' => 'array',
-				'paths' => [__DIR__ . '/../src/Entity']
-			],
-			'orm_default' => [
-				'drivers' => [
-					__NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-				]
-			]
-		]
-	]  
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ . '_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../src/Entity']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ]
+            ]
+        ]
+    ],
 ];
